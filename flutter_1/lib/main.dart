952a446fecff74a38474446a3acedee4a452a6a0
Jilvo <jmillot41@gmail.com>
2025-06-12 17:47:50 +0200

    add commande section

--- flutter_1/lib/main.dart
+++ flutter_1/lib/main.dart
@@ -26,19 +26,84 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        backgroundColor: Colors.pink[100],
+        toolbarHeight: 200,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text('Burger Queen'),
-            const Icon(Icons.fastfood), // Icône de burger
-            const CircleAvatar(
-              backgroundImage: AssetImage('assets/avatar.png'), // Avatar
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Icon(Icons.fastfood, size: 28),
+                Text(
+                  'Burger Queen',
+                  style: TextStyle(
+                    color: Colors.brown,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
+                  ),
+                ),
+                Icon(Icons.account_circle, size: 28),
+              ],
+            ),
+            SizedBox(height: 30), // espacement
+            const Card(
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.location_on, size: 24),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            'Mon restaurant le plus proche',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Spacer(),
+                        Text("4kms"),
+                      ],
+                    ),
+                    SizedBox(height: 1),
+                    Card(
+                      color: Color.fromRGBO(234, 30, 99, 1),
+                      child: Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.touch_app,
+                                size: 24,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                'Commander',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
       body: const Center(
-        child: Text('Contenu principal'),
+        child: Text('En ce moment', style: TextStyle(color: Colors.brown)),
       ),
     );
   }
